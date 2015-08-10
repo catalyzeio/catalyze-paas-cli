530c3e98f5672cf85f987140d55f313c6778c152
Nicholas Mistry <nick@catalyze.io>
2015-08-09 22:50:04 -0500

    adds sparklines, but unfinished.

--- catalyze/commands/metrics.py
+++ catalyze/commands/metrics.py
@@ -5,6 +5,7 @@ import sys
 import json
 import time
 import math
+import sparkline
 import csv
 from StringIO import StringIO
 from catalyze import cli, client, project, output
@@ -12,7 +13,7 @@ from catalyze.helpers import environments, services
 
 @cli.command("metrics", short_help = "Get service metrics")
 @click.argument("service_label", required = False, default = None)
-@click.option("--format", help = "Output in a special format. Accepted values are 'csv' and 'json'.")
+@click.option("--format", help = "Output in a special format. Accepted values are 'csv', 'json', 'spark'.")
 @click.option("--stream", is_flag = True, default = False, help = "Repeat calls once per minute until this process is interrupted.")
 @click.option("--mins", type = int, default = 1, help = "How many minutes' worth of logs to retrieve.")
 def metrics(service_label, format, stream, mins):
@@ -26,6 +27,9 @@ def metrics(service_label, format, stream, mins):
         transformer = CSVTransformer()
     elif format == "json":
         transformer = JSONTransformer()
+    elif format == "spark":
+        transformer = SparkTransformer()
+        mins = 60
     else:
         output.error("unrecognized format '%s'" % (format,))
 
@@ -137,3 +141,38 @@ class CSVTransformer(MetricsTransformer):
         for service in data:
             self.transform_single(service["jobs"], service["serviceId"], service["serviceName"])
         output.write(self.sio.getvalue())
+
+def deltas(l): 
+    """
+    Compute the change between each datapoint.  Since there will be n-1 deltas, 
+    add a zero to the front.  While not perfectly accurate, the leading zero
+    sets the minimum to always be zero.
+    """
+     if l:
+        return [0] + [j-i for i,j in  zip(l[:-1], l[1:])]
+     else:
+        return []
+
+class SparkTransformer(MetricsTransformer):
+    def transform_single(self, data, prefix = ""):
+        for job in data:
+            metrics_reversed = job["metrics"][::-1]
+            sparkdata = {}
+            sparkdata["CPU"] = [metric["cpu"]["usage"]/1000000000.0/60.0*100.0 for metric in metrics_reversed]
+            sparkdata["Memory"] =  [metric["memory"]["ave"] for metric in metrics_reversed]
+            sparkdata["Net RX"] = deltas([metric["network"]["rx_bytes"]["ave"] for metric in metrics_reversed])
+            sparkdata["Net TX"] = deltas([metric["network"]["tx_bytes"]["ave"] for metric in metrics_reversed])
+            sparkdata["Disk Read"] = [metric["diskio"]["read"] for metric in metrics_reversed]
+            sparkdata["Disk Write"] = [metric["diskio"]["write"] for metric in metrics_reversed]
+
+            for k in sorted(sparkdata.keys()):
+                v  = sparkdata[k]
+                if v:
+                    spark = sparkline.sparkify(v).encode('utf-8')
+                    output.write("{}{:>10}: {} min: {} max: {}".format(prefix,k,spark,min(v),max(v)))
+
+    def transform_group(self, data):
+        for service in data:
+            output.write(service["serviceName"] + ":")
+            self.transform_single(service["jobs"], prefix = "    ")
+
